809bfaa8bc469b2bcec7d5b5d7c9f852ee31f87f
Charles Evans <charles.evans@instaclustr.com>
2020-12-29 10:56:49 +1100

    Updated deprecated readLine method

--- core/src/main/scala/kafka/admin/IcTopicCommand.scala
+++ core/src/main/scala/kafka/admin/IcTopicCommand.scala
@@ -59,6 +59,7 @@ import org.apache.zookeeper.KeeperException.NodeExistsException
 
 import scala.collection.JavaConverters._
 import scala.collection._
+import scala.io.StdIn
 
 object IcTopicCommand extends Logging {
 
@@ -485,7 +486,7 @@ object IcTopicCommand extends Logging {
 
   def askToProceed(): Unit = {
     println("Are you sure you want to continue? [y/n]")
-    if (!Console.readLine().equalsIgnoreCase("y")) {
+    if (StdIn.readLine().equalsIgnoreCase("y")) {
       println("Ending your session")
       Exit.exit(0)
     }
